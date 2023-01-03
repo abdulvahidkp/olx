0e9b9deaf512e9e7dfdec71a83c4f9fd472b0f9c
Abdul Vahid K P <abdulvahid1117@gmail.com>
2023-01-03 20:51:02 +0530

    signup route set and state created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+      <Router>
+          <Route exact component={Home} path="/" />
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/signup" component={Signup} />
+      </Router>
   );
 }
 
